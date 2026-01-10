7b353c8d5a6522762d8cf2f77f52df2702e96108
RoshanAmity <roshan.ansari.8076@gmail.com>
2026-01-10 16:53:05 +0530

    remove frontend

--- frontend/src/pages/collections/Collections.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { allProducts } from "../../assets/MockData";
-import CollectionPage from "../../components/collections/CollectionPage";
-
-const Collections = () => {
-  return <CollectionPage title="Collections" products={allProducts} />;
-};
-
-export default Collections;
