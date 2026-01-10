666a7d251f674a65db7dc27bde1f1be115711eac
RoshanAmity <roshan.ansari.8076@gmail.com>
2026-01-10 21:22:38 +0530

    auth almost finish

--- backend/src/controllers/customer/auth.controllers.js
+++ backend/src/controllers/customer/auth.controllers.js
@@ -1,11 +1,12 @@
 import auth_Model from "../../models/customer/user.model.js";
-import {ApiError} from "../../utils/api-error.js";
-import {ApiResponse} from "../../utils/api-response.js";
+import { ApiError } from "../../utils/api-error.js";
+import { ApiResponse } from "../../utils/api-response.js";
 import { encryptPasswordMethod, decryptPasswordMethod } from "../../utils/passwordEncrypt&passwordDecrypt.js";
+import cookiesForUser from "../../utils/cookiesForUser.js";
 
 const Signup = async (req, res) => {
-    try{
-        const {firstName, lastName, email, password} = req.body;
+    try {
+        const { firstName, lastName, email, password } = req.body;
 
         const customerDetail = auth_Model({
             email: email,
@@ -19,45 +20,56 @@ const Signup = async (req, res) => {
         customerDetail.password = undefined;
         customerDetail.contact = undefined;
 
+        cookiesForUser(res, customerDetail)
+
         return res.status(200).json(new ApiResponse(200, null, "Registration Successful"));
     }
-    catch(err){
-        return res.status(500).json(new ApiError(500, err.message, [{message: err.message, name: err.name}]));
+    catch (err) {
+        return res.status(500).json(new ApiError(500, err.message, [{ message: err.message, name: err.name }]));
     }
 }
 
 const Login = async (req, res) => {
-    try{
-        const {email, password} = req.body;
+    try {
+        const { email, password } = req.body;
 
-        const customerDetail = await auth_Model.findOne({email: email});
+        const customerDetail = await auth_Model.findOne({ email: email });
 
         const decryptPassword = decryptPasswordMethod(password, customerDetail.password);
 
-        if(!decryptPassword){
+        if (!decryptPassword) {
             return res.status(401).json(new ApiError(401, "Incorrect Password"));
         }
 
+        customerDetail.password = undefined;
+        customerDetail.contact = undefined;
+
+        cookiesForUser(res, customerDetail)
         return res.status(200).json(new ApiResponse(200, null, "Access Granted"));
     }
-    catch(err){
-        return res.status(500).json(new ApiError(500, err.message, [{message: err.message, name: err.name}]));
+    catch (err) {
+        return res.status(500).json(new ApiError(500, err.message, [{ message: err.message, name: err.name }]));
     }
 }
 
 const ForgotPassword = async (req, res) => {
-    try{
-        const {email, password} = req.body;
+    try {
+        const { email, password } = req.body;
 
         let customerDetail = await auth_Model.findOneAndUpdate(
-            {email:email},
+            { email: email },
             {
-                password:  await encryptPasswordMethod(password)
+                password: await encryptPasswordMethod(password)
             }
         );
 
+        customerDetail.password = undefined;
+        customerDetail.contact = undefined;
+
+        cookiesForUser(res, customerDetail)
+
     }
-    catch(err){
-        return res.status(500).json(new ApiError(500, err.message, [{message: err.message, name: err.name}]));
+    catch (err) {
+        return res.status(500).json(new ApiError(500, err.message, [{ message: err.message, name: err.name }]));
     }
 }
